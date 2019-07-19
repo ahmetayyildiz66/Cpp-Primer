22374d3d81f15399ed3cc1be70635659bb8d9114
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2019-07-19 15:02:34 +0300

    002 - IO Example

---
+++ 002-IO/IO.cpp
@@ -0,0 +1,17 @@
+/*
+ * IO.cpp
+ *
+ *  Created on: Jul 19, 2019
+ *      Author: MONSTER
+ */
+#include <iostream>
+int main(){
+	std::cout<<"Hello, World"<<std::endl;
+	int v1 = 0, v2 = 0, sum = 0;
+	std::cin>>v1>>v2;
+	sum = v1 * v2;
+	std::cout<<"v1 is "<<v1 <<" v2 is " << v2 <<" sum is " <<sum;
+	return 0;
+}
+
+
